c68d930e3216422e5233d753062b058c188ac338
Matt Kerbawy <matt@pendo.io>
2017-03-06 12:00:05 -0500

    add wire events snippet

---
+++ wireEvents.js
@@ -0,0 +1,9 @@
+// Wire events to elements
+(function wireGuideAdvanceButton (step) {
+    step && step.attachEvent(step.guideElement[0], 'click', function (e) {
+        var advanceButton = pendo.dom(e.target || e.srcElement).closest('._pendo-guide-next_');
+        if (advanceButton.length) {
+            pendo.onGuideAdvanced();
+        }
+    });
+})(step,guide);

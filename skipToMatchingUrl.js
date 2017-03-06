29d4b1cc260a41d815e2fa3d85a34d7f5b4d615b
Matt Kerbawy <matt@pendo.io>
2017-03-06 12:06:57 -0500

    add skip to page match

---
+++ skipToMatchingUrl.js
@@ -0,0 +1,26 @@
+pendo._.defer(function (){
+    // Skip to first step which matches current URL if step assigned to specific page
+    var url = pendo.getCurrentUrl();
+    var currentGuide = guide;
+    if (!currentGuide) {return;}
+    var steps = currentGuide.steps;
+    // Ignore any steps which are sitewide
+    var testSteps = pendo._.filter(pendo._.rest(steps), function(step){
+        return step.pageId;
+    });
+    // TODO handle guide with all sitewide steps
+    // Return step array index of first page regex match to current url
+    var startingPoint = pendo._.indexOf(steps, pendo._.find(testSteps, function(step){
+        return pendo.testUrlForStep(step.regexUrlRule, url);
+    }));
+    pendo.log(guide.id + ":startingPoint is "+startingPoint);
+    // If no matches found
+    if (startingPoint == -1) {
+        pendo.onGuideAdvanced();
+        return;
+    }
+    // Based on previous logic startingPoint should be > 0
+    var prevStep = steps[startingPoint-1];
+    pendo.log(guide.id + ':found starting step to be '+prevStep.id);
+    pendo.onGuideAdvanced(prevStep);
+}, 1);

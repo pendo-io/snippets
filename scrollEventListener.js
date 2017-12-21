801bd673a271a58f0aa6ba856b3dc0834141c15e
Spencer Harwood <spencer@pendo.io>
2017-12-21 12:02:34 -0500

    Clarify the scrolled to bottom in code and added a throttle to limit the amount of event function triggers.

--- scrollEventListener.js
+++ scrollEventListener.js
@@ -1,8 +1,8 @@
-(function wireGuideScrollAction(step) {
-    step && step.attachEvent(step.guideElement[0], 'scroll', function (e) {
+(function wireGuideScrollToBottom(step) {
+    step && step.attachEvent(step.guideElement[0], 'scroll', pendo._.throttle(function onScroll(e) {
         var scrollable = pendo.Sizzle('.article-body');
         if(scrollable[0].offsetHeight + scrollable[0].scrollTop == scrollable[0].scrollHeight) {
-				      console.log('SCROLLED');
+				      console.log('SCROLLED TO BOTTOM');
 		    }
-    });
+    }, 150));
 })(step,guide);

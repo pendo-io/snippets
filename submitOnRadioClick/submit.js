a2ef1adb264b60f4556d687876766d93a07ace03
Josh Connelly <31379011+connellyguy@users.noreply.github.com>
2020-03-02 14:28:05 -0500

    removed arrow functions for backward-compat

--- submitOnRadioClick/submit.js
+++ submitOnRadioClick/submit.js
@@ -1,8 +1,8 @@
 (function() {
     var submitButtonId = 'pendo-button-d661c167';
     pendo.dom('#' + submitButtonId).addClass('_pendo-custom-hidden');
-    pendo.dom('[class^=_pendo-number-scale-poll]').on('click', () => {
-        setTimeout(() => {
+    pendo.dom('[class^=_pendo-number-scale-poll]').on('click', function() {
+        setTimeout(function() {
             document.getElementById(submitButtonId).click();
         }, 100);
     });

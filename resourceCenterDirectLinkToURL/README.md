67ce0298597302ca6e487097bfc10113b1124872
connellyguy <joshua@joshc.net>
2020-03-02 14:21:40 -0500

    added commonly requested 'redirect to URL' code

---
+++ resourceCenterDirectLinkToURL/README.md
@@ -0,0 +1,7 @@
+# Direct External Link
+
+## What does it do?
+Make a Resource Center code sandbox module immediately open an external link in a new tab and close the resource center
+
+## What do I need to do to make this snippet work?
+Just replace the example.com URL with your desired URL, then place the whole snippet in a code sandbox resource center module
---
+++ resourceCenterDirectLinkToURL/js.js
@@ -0,0 +1,5 @@
+(function resourceCenterOpenURL() {
+    document.getElementById('pendo-resource-center-container').setAttribute('style', 'display:none');
+    window.open("https://example.com/",'_blank'); /* Replace https://example.com/ with your desired external URL */
+    window.pendo.BuildingBlocks.BuildingBlockResourceCenter.getResourceCenter().hide();
+ })();

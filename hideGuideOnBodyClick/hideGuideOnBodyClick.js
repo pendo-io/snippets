76ded8e46bdbc45a2e0c5af36f8c744ed9ab7f3b
Josh Connelly <31379011+connellyguy@users.noreply.github.com>
2020-03-13 11:03:51 -0400

    add event listener clear

--- hideGuideOnBodyClick/hideGuideOnBodyClick.js
+++ hideGuideOnBodyClick/hideGuideOnBodyClick.js
@@ -1,13 +1,20 @@
 (function hideGuideOnBodyClick(dom) {
-    dom(document).on('click', 'body', function hideGuide(e) {
+    function hideGuide(e) {
         if (
             !dom(eventTarget(e)).closest('#pendo-guide-container').length
         ) {
             pendo.onGuideDismissed();
         }
-    });
+    };
+    
+    pendo.attachEvent(document, 'click', hideGuide);
     
     function eventTarget (e) {
         return (e && e.target) || e.srcElement;
     }
+    
+    // step wrappable method to clear all event listeners
+    step.after('teardown', function () {
+        pendo.detachEvent(document, 'click', hideGuide);
+    });
 })(pendo.dom);

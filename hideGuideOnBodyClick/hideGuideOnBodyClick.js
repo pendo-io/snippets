0ac6241f874a05bd24fd58083e60f462b0736b3f
Josh Connelly <31379011+connellyguy@users.noreply.github.com>
2020-03-13 10:46:10 -0400

    wrap in IIFE

--- hideGuideOnBodyClick/hideGuideOnBodyClick.js
+++ hideGuideOnBodyClick/hideGuideOnBodyClick.js
@@ -1,11 +1,13 @@
-pendo.dom(document).on('click', 'body', function hideGuide(e) {
-    if (
-        !dom(eventTarget(e)).closest('#pendo-guide-container').length
-    ) {
-        pendo.onGuideDismissed();
+(function hideGuideOnBodyClick() {
+    dom(document).on('click', 'body', function hideGuide(e) {
+        if (
+            !dom(eventTarget(e)).closest('#pendo-guide-container').length
+        ) {
+            pendo.onGuideDismissed();
+        }
+    });
+    ​
+    function eventTarget (e) {
+        return (e && e.target) || e.srcElement;
     }
-});
-​
-function eventTarget (e) {
-    return (e && e.target) || e.srcElement;
-}
+})(pendo.dom);

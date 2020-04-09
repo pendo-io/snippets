019246461c26859c25f1456b3c8b0c90e5902523
connellyguy <joshua@joshc.net>
2020-04-09 13:30:12 -0400

    fixed suggested tag

--- hideRCOnBodyClick/rc-offclick.js
+++ hideRCOnBodyClick/rc-offclick.js
@@ -1,5 +1,5 @@
 /*
-Put this code snippet in a custom code block on a tooltip guide pointing to the element '.pendo-resource-center-badge-container'
+Put this code snippet in a custom code block on a tooltip guide pointing to the element '._pendo-resource-center-badge-container'
 Set the guide to display on 'Target Element', with the settings 'display on hover or click' and 'display every time'
 Add this line to the CSS tab of the custom code block on the guide:
 display: none !important;

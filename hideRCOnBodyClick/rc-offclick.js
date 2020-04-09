e2c25cf1a921d12e82327ecbebad9315e0ef4a10
connellyguy <joshua@joshc.net>
2020-04-09 13:24:18 -0400

    added rc-offclick snippet

---
+++ hideRCOnBodyClick/rc-offclick.js
@@ -0,0 +1,22 @@
+/*
+Put this code snippet in a custom code block on a tooltip guide pointing to the element '.pendo-resource-center-badge-container'
+Set the guide to display on 'Target Element', with the settings 'display on hover or click' and 'display every time'
+Add this line to the CSS tab:
+display: none !important;
+
+End result: The 2.0 Pendo Resource Center will hide itself when a click is registered outside of the resource center container
+*/
+(function offclickRC(dom) {
+    if (!pendo.designerEnabled) {
+        //window.alert("The Resource Center badge was hovered over.");
+		function hideRC(e) {
+        	var tgt = e.target || e.srcElement;
+            console.log(dom(tgt));
+            if (!dom(tgt).closest('#pendo-resource-center-container').length) {
+                window.pendo.BuildingBlocks.BuildingBlockResourceCenter.getResourceCenter().hide();
+            }
+        }
+        pendo.attachEvent(document, 'click', hideRC);
+    }
+    pendo.onGuideDismissed();
+})(pendo.dom);

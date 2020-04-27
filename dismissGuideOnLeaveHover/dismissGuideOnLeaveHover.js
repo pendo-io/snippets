b34c2b7158cabfe9e09ff6a0dbb6412dd4fa8e30
connellyguy <joshua@joshc.net>
2020-04-27 14:03:39 -0400

    added hover dismiss snippet

---
+++ dismissGuideOnLeaveHover/dismissGuideOnLeaveHover.js
@@ -0,0 +1,16 @@
+/*
+* By default, guides set to appear on hovering a target element do not dismiss when the visitor hovers off the target element
+* This snippet enables that behavior for target elements
+*/
+
+(function dismissGuideOnLeaveHover(dom) {
+    function mouseoverHandler(e) {
+        var tgt = e.target || e.srcElement;
+        if (!dom(tgt).closest(step.elementPathRule).length) {
+            pendo.onGuideAdvanced();
+            pendo.detachEvent(window, 'mouseover', mouseoverHandler);
+        }
+    }
+
+	pendo.attachEvent(window, 'mouseover', mouseoverHandler);
+})(pendo.dom)

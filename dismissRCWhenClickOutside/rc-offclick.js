191a6be25b7f68802a20178544f73e902050afca
Josh Connelly <31379011+connellyguy@users.noreply.github.com>
2020-05-06 23:28:06 -0400

    Removed onGuideDismissed
    Something in the agent changed so this was now dismissing the Resource Center as well.

--- dismissRCWhenClickOutside/rc-offclick.js
+++ dismissRCWhenClickOutside/rc-offclick.js
@@ -11,12 +11,10 @@ End result: The 2.0 Pendo Resource Center will hide itself when a click is regis
         //window.alert("The Resource Center badge was hovered over.");
 		function hideRC(e) {
         	var tgt = e.target || e.srcElement;
-            console.log(dom(tgt));
             if (!dom(tgt).closest('#pendo-resource-center-container').length) {
                 window.pendo.BuildingBlocks.BuildingBlockResourceCenter.getResourceCenter().hide();
             }
         }
         pendo.attachEvent(document, 'click', hideRC);
     }
-    pendo.onGuideDismissed();
 })(pendo.dom);

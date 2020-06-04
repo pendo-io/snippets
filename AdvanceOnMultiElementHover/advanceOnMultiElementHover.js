96ac586a03d48b38941d9e423101d8cc4e6a93cf
Josh Connelly <31379011+connellyguy@users.noreply.github.com>
2020-06-04 15:31:56 -0400

    Update advanceOnMultiElementHover.js

--- AdvanceOnMultiElementHover/advanceOnMultiElementHover.js
+++ AdvanceOnMultiElementHover/advanceOnMultiElementHover.js
@@ -1,9 +1,10 @@
 (function advanceGuideOnHover(dom) {
     var advanceOnce = pendo._.once(pendo.onGuideAdvanced); // make sure the guide only advances one time
-
-    var one = dom('#id-number-1');
+    
+    // You can add as many element rules as you like in a list here
+    var one = dom('#id-number-1'); // These element rules can be an id like #myId or a class like .myClass
     var two = dom('#id-number-2');
-    var elms = [one, two];
+    var elms = [one, two]; // if you add more elements to the list, you must also add them to the array
 
     elms.forEach(function(elm) {
         pendo.attachEvent(dom(elm)[0], 'mouseover', advanceOnce);

c669fb557bfbb570416d618bc7b905332418a6a6
Josh Connelly <31379011+connellyguy@users.noreply.github.com>
2020-06-04 12:13:47 -0400

    Update js.js

--- resourceCenterOpenURL/js.js
+++ resourceCenterOpenURL/js.js
@@ -1,5 +1,7 @@
 (function resourceCenterOpenURL() {
-    document.getElementById('pendo-resource-center-container').setAttribute('style', 'display:none');
-    window.open("https://example.com/",'_blank'); /* Replace https://example.com/ with your desired external URL */
-    window.pendo.BuildingBlocks.BuildingBlockResourceCenter.getResourceCenter().hide();
+    if (!pendo.designerEnabled) {
+        document.getElementById('pendo-resource-center-container').setAttribute('style', 'display:none');
+        window.open("https://example.com/",'_blank'); /* Replace https://example.com/ with your desired external URL */
+        window.pendo.BuildingBlocks.BuildingBlockResourceCenter.getResourceCenter().hide();
+    }
  })();

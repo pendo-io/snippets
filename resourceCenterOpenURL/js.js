0b9bebc9259b4febcd3fa6238594a6b124c0735f
Josh Connelly <31379011+connellyguy@users.noreply.github.com>
2020-09-18 13:33:19 -0400

    Update js.js
    add line to return to RC home

--- resourceCenterOpenURL/js.js
+++ resourceCenterOpenURL/js.js
@@ -2,6 +2,9 @@
     if (!pendo.designerEnabled) {
         document.getElementById('pendo-resource-center-container').setAttribute('style', 'display:none');
         window.open("https://example.com/",'_blank'); /* Replace https://example.com/ with your desired external URL */
+        step.eventRouter.eventable.trigger('pendoEvent', {
+            action: 'returnToResourceCenterHome'
+        });
         window.pendo.BuildingBlocks.BuildingBlockResourceCenter.getResourceCenter().hide();
     }
  })();

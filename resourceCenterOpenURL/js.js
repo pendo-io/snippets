06fb0738e0060a7109c1168cb50e96cd0347232f
Josh Connelly <31379011+connellyguy@users.noreply.github.com>
2020-10-01 10:10:06 -0400

    fixed quotes

--- resourceCenterOpenURL/js.js
+++ resourceCenterOpenURL/js.js
@@ -1,9 +1,9 @@
 (function resourceCenterOpenURL() {
     if (!pendo.designerEnabled) {
-        document.getElementById(‘pendo-resource-center-container’).setAttribute(‘style’, ‘display:none’);
-        window.open(“https://example.com/”,‘_blank’); /* Replace https://example.com/ with your desired external URL */
-        step.eventRouter.eventable.trigger(‘pendoEvent’, {
-            action: ‘returnToResourceCenterHome’
+        document.getElementById('pendo-resource-center-container').setAttribute('style', 'display:none');
+        window.open("https://example.com/",'_blank'); /* Replace https://example.com/ with your desired external URL */
+        step.eventRouter.eventable.trigger('pendoEvent', {
+            action: 'returnToResourceCenterHome'
         });
         setTimeout(function(){
             pendo.onGuideDismissed(pendo.BuildingBlocks.BuildingBlockResourceCenter.getResourceCenter().steps[0]);

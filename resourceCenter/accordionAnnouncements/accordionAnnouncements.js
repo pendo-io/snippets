ac2c7ae230672262335be8abea8e2daa91cdef85
Mike Fotinatos <mikefo@pendo.io>
2021-04-15 07:07:49 -0400

    fixup to accordion announcements

--- resourceCenter/accordionAnnouncements/accordionAnnouncements.js
+++ resourceCenter/accordionAnnouncements/accordionAnnouncements.js
@@ -1,12 +1,13 @@
 (function (step, guide) {
     // Retrieve StepID from step object
-    var stepId = step.guideId;
-    
+    var stepId = step.id;
+    var guideId = step.guideId;
+
     var target = pendo.dom("#pendo-resource-center-container")[0];
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if (mutation.addedNodes.length) {
-                createAccordions(stepId);
+                createAccordions(stepId, guideId);
                 target.classList.add('_pendo-acc-enabled');
             }
         })
@@ -24,15 +25,25 @@
     }
 })(step, guide);
 
-function createAccordions(stepId){
+function createAccordions(stepId, guideId){
+   var validId = "";
    if(pendo.dom("#pendo-g-" + stepId + " #pendo-guide-container")[0]) {
+        validId = stepId;
+   }
+
+   if(pendo.dom("#pendo-g-" + guideId + " #pendo-guide-container")[0]) {
+        validId = guideId;
+   }
+
+   if(pendo.dom("#pendo-g-" + validId + " #pendo-guide-container")[0]) {
         // Get first element which will be the header
 
-        var announcementHeader = pendo.dom("#pendo-g-" + stepId + " #pendo-guide-container")[0].children[0];
+        var announcementHeader = pendo.dom("#pendo-g-" + validId + " #pendo-guide-container")[0].children[0];
         announcementHeader.style.cursor = "pointer";
+        announcementHeader.classList.add('_pendo-acc-collapsed');
 
         // Get rest of elements for accordion
-        var accordionContents = [].slice.call(pendo.dom("#pendo-g-" + stepId + " #pendo-guide-container")[0].children).slice(1);
+        var accordionContents = [].slice.call(pendo.dom("#pendo-g-" + validId + " #pendo-guide-container")[0].children).slice(1);
 
         pendo._.each(accordionContents, function(elem) {
             if(elem.classList.contains('_pendo-row')){
@@ -46,10 +57,11 @@ function createAccordions(stepId){
         announcementHeader.addEventListener('click', expandable, false);
 
         function expandable() {
-            accordionContents = [].slice.call(pendo.dom("#pendo-g-" + stepId + " #pendo-guide-container")[0].children).slice(1);
+            accordionContents = [].slice.call(pendo.dom("#pendo-g-" + validId + " #pendo-guide-container")[0].children).slice(1);
             if(!announcementHeader.classList.contains('_pendo-acc-active')) {
-                pendo.dom("#pendo-g-" + stepId)[0].style.height = "auto";
+                pendo.dom("#pendo-g-" + validId)[0].style.height = "auto";
                 announcementHeader.classList.add('_pendo-acc-active');
+                announcementHeader.classList.remove('_pendo-acc-collapsed');
                 pendo._.each(accordionContents, function(elem) {
                     if(elem.classList.contains('_pendo-row')){
                         elem.children[0].style.display = "block";
@@ -59,6 +71,7 @@ function createAccordions(stepId){
                 });
             } else {
                 announcementHeader.classList.remove('_pendo-acc-active');
+                announcementHeader.classList.add('_pendo-acc-collapsed');
                 pendo._.each(accordionContents, function(elem) {
                     if(elem.classList.contains('_pendo-row')){
                         elem.children[0].style.display = "none";

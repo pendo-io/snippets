6891299677d8e37f5ca331db32d66acf041bcdae
Kristine Rosette Enerio <kristine.enerio@kristine.enerio>
2021-09-20 14:08:50 -0400

    Adding more explanation to the snippetExplanation.js

--- guides/snippetExplanation/snippetExplanation.js
+++ guides/snippetExplanation/snippetExplanation.js
@@ -1,4 +1,4 @@
-//Add this to the top of your code snippet
+//Add this to the top of your code snippet.  Don't forget to declare pendo.dom at the end of your containing function.
 var codeExplanation = dom("code-explanation");
 if (!pendo.designerEnabled) {
 	codeExplanation.addClass("pendo-hide");

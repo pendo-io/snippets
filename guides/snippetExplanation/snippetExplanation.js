50adb76ded556f1d080bce4bd547e2a43aee92c0
Kristine Rosette Enerio <kristine.enerio@kristine.enerio>
2021-09-20 16:36:59 -0400

    Adding some js for codeblocks that are in a step you want to be visible

--- guides/snippetExplanation/snippetExplanation.js
+++ guides/snippetExplanation/snippetExplanation.js
@@ -1,5 +1,9 @@
 //Add this to the top of your code snippet.  Don't forget to declare pendo.dom at the end of your containing function.
 var codeExplanation = dom(".code-explanation");
-if (!pendo.designerEnabled) {
-	codeExplanation.addClass("pendo-hide");
-};
+//Uncomment the following line if your code block is added to a step that you want the end user to see
+//var stepContainerSize = dom("._pendo-step-container-size")[0];
+	if (!pendo.designerEnabled) {
+		codeExplanation.addClass("pendo-hide");
+		//Uncomment the following line if your code block is added to a step that you want the end user to see
+        //stepContainerSize.style.removeProperty("height");
+	};

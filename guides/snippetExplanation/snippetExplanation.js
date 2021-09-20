87e8797e0aae7911af2d767ecadc29250623dc82
Kristine Rosette Enerio <kristine.enerio@kristine.enerio>
2021-09-20 15:54:24 -0400

    Fixing element selector

--- guides/snippetExplanation/snippetExplanation.js
+++ guides/snippetExplanation/snippetExplanation.js
@@ -1,5 +1,5 @@
 //Add this to the top of your code snippet.  Don't forget to declare pendo.dom at the end of your containing function.
-var codeExplanation = dom("code-explanation");
+var codeExplanation = dom(".code-explanation");
 if (!pendo.designerEnabled) {
 	codeExplanation.addClass("pendo-hide");
 };

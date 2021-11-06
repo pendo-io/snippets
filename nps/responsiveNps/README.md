9c72b6ef9ce401588791704af5927347065d44fa
Mike Fotinatos <mikefo@pendo.io>
2021-11-05 20:08:49 -0400

    adding responsive NPS

---
+++ nps/responsiveNps/README.md
@@ -0,0 +1,7 @@
+# Responsive NPS Module
+
+[Video Demo](https://drive.google.com/file/d/1Zl1yWAmV7AwSWJioMRkaKGBfE5QgvbJ6/view?usp=sharing)
+
+Far from perfect, but should render respectably on most mobile web screens:
+![Responsive NPS](https://i.imgur.com/uySP2YN.png)
+
---
+++ nps/responsiveNps/responsiveNps.js
@@ -0,0 +1,5 @@
+if(!pendo.designerEnabled) {
+    pendo.dom("#pendo-guide-container")[0].parentElement.style.height="";
+    pendo.dom("._pendo-nps-poll-upperbound-label")[0].style.display = "contents";
+    pendo.dom("._pendo-nps-open-text-poll-input")[0].style.width = "100%";
+}

32373e39a0ca64c7d910e72e0c133804e5bc14bc
John Rix <41085020+john-rix@users.noreply.github.com>
2021-11-11 10:08:22 -0500

    updating collapse code

--- guides/customGuideAssets/collapsibleGuide/collapsibleGuide.js
+++ guides/customGuideAssets/collapsibleGuide/collapsibleGuide.js
@@ -1,33 +1,31 @@
-//This is designed to work with a bottom-aligned banner guide with a header section at the top which will remain visible when the guide collapses
-//You could also collapse the guide all the way so that only the collapse button remains visible at the bottom of the page. The amount that the guide collapses (moves down) is controlled by the “offset” variable
-//The collapsed state is saved in sessionStorage so that when users navigate between pages, the guide will stay collapsed or expanded
+// This customization is designed to let the guide container "collapse" and move down below the fold and out of the way.
+// It works best with content-heavy guides that the user can reference when they need to and collapse when they want to access the application UI behind the guide.
+// The collapsed state is saved in sessionStorage so that when users navigate between pages, the guide will stay collapsed or expanded.
 
 (function (dom) {
-  var offset = '276px' //edit this value based on the height of your guide minus the height of the header you want to remain visible
-  
-  //create collapse/expand button with arrow inside
+  var offset = '175px' // ***EDIT THIS VALUE BASED ON HOW FAR DOWN THE GUIDE NEEDS TO MOVE***
   var collapseHtml = `<div class="pendo-expand-collapse-button"><span class="pendo-expand-collapse-arrow"></span></div>`;
   var collapseButton = document.createElement("div");
   collapseButton.innerHTML = collapseHtml;
   var guideContainer = document.getElementById("pendo-guide-container");
   guideContainer.appendChild(collapseButton);
 
-  //check sessionStorage for initial collapse state
+// Check sessionStorage for initial collapse state
+if (sessionStorage.pendoCollapseStatus === "collapse") {
+  guideContainer.style.top = offset;
+  guideContainer.classList.add("collapse");
+}
+
+// Toggle collapse/expand state on button click
+dom(".pendo-expand-collapse-button").on("click", function () {
   if (sessionStorage.pendoCollapseStatus === "collapse") {
+    guideContainer.style.top = "0px";
+    guideContainer.classList.remove("collapse");
+    sessionStorage.pendoCollapseStatus = "expand";
+  } else {
     guideContainer.style.top = offset;
     guideContainer.classList.add("collapse");
+    sessionStorage.pendoCollapseStatus = "collapse";
   }
-
-  //toggle collapse/expand state on button click
-  dom(".pendo-expand-collapse-button").on("click", function () {
-    if (sessionStorage.pendoCollapseStatus === "collapse") {
-      guideContainer.style.top = "0px";
-      guideContainer.classList.remove("collapse");
-      sessionStorage.pendoCollapseStatus = "expand";
-    } else {
-      guideContainer.style.top = offset;
-      guideContainer.classList.add("collapse");
-      sessionStorage.pendoCollapseStatus = "collapse";
-    }
-  });
+});
 })(pendo.dom);

ed03d737b457323f99c46690a4e3aadb8a4c8dd5
Bryndon Humphrey <64496396+bryndonhumphrey@users.noreply.github.com>
2022-08-10 15:04:35 -0700

    Update reloadingGuide.js
    cleanup

--- guides/guideFrequency/reloadingGuide/reloadingGuide.js
+++ guides/guideFrequency/reloadingGuide/reloadingGuide.js
@@ -1,4 +1,4 @@
-(function reloadingGuide(dom) {
+(function reloadingGuide() {
     if(!pendo.designerEnabled) {
         var old_element = document.querySelectorAll('._pendo-close-guide')[0];
         var new_element = old_element.cloneNode(true);

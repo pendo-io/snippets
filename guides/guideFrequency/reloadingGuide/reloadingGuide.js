d0d0e4761cee510cb2943676fddadd3cb645e19f
Bryndon Humphrey <64496396+bryndonhumphrey@users.noreply.github.com>
2022-08-10 14:59:10 -0700

    Update reloadingGuide.js
    wasn't working for ultimate software, updated to make work.

--- guides/guideFrequency/reloadingGuide/reloadingGuide.js
+++ guides/guideFrequency/reloadingGuide/reloadingGuide.js
@@ -1,11 +1,16 @@
 (function reloadingGuide(dom) {
     if(!pendo.designerEnabled) {
-        dom('._pendo-close-guide')[0].outerHTML = dom('._pendo-close-guide')[0].outerHTML; // remove direct event listener on close button
+        var old_element = document.querySelectorAll('._pendo-close-guide')[0];
+        var new_element = old_element.cloneNode(true);
+        old_element.parentNode.replaceChild(new_element, old_element);
+        
+        var closeButton = document.querySelectorAll('._pendo-close-guide')[0]
         
         function dismissUntilReload() {
             // guide dismissed until reload, at which point it will no longer be in the payload
             pendo.onGuideDismissed({"until":"reload"});
         }
-        pendo.attachEvent(dom('._pendo-close-guide')[0], 'click', dismissUntilReload);
+        
+        pendo.attachEvent(closeButton, 'click', dismissUntilReload);
     }
-})(pendo.dom);
+})();

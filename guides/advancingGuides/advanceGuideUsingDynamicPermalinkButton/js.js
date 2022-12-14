0262a5615cf407b0756442400a1535c6d5223eab
rohit366 <15645502+rohit366@users.noreply.github.com>
2022-12-14 15:04:31 +0530

    Update js.js

--- guides/advancingGuides/advanceGuideUsingDynamicPermalinkButton/js.js
+++ guides/advancingGuides/advanceGuideUsingDynamicPermalinkButton/js.js
@@ -2,16 +2,16 @@
     step && step.attachEvent(step.guideElement[0], 'click', function(e) {
         var advanceButton = pendo.dom(e.target || e.srcElement).closest('.opt1');
         if (advanceButton.length) {
-            // get the text value of the fetched metadata to be used ahead.
+            // get the text value from the metadata to be used ahead in the URL.
             var account_name = document.getElementsByClassName("_pendo-hidden-account_")[0].innerHTML
 
-            // build your URL by using the variable above.
+            // building your URL by using the metadata text value.
             var newurl = "https://" + account_name + ".abm.6sense.com/settings/integration/manage?pendo=LvGZXdsqwUbxZKzkqAvXlCZkDgc"
 
             //Finally dismissing the current guide to trigger the permalink.
             pendo.onGuideDismissed();
 
-            // Opening the dynamic permalink url in the same browser tab.
+            // Opening the dynamic permalink url in the same browser tab after refreshing the page.
             window.open(newurl, "_self");
         }
     });

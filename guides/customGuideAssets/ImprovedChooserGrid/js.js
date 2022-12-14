1bd77df0100073159750ddb9c2a3703259ccd7e4
rohit366 <15645502+rohit366@users.noreply.github.com>
2022-12-14 17:42:11 +0530

    new addition
    new addition

---
+++ guides/customGuideAssets/ImprovedChooserGrid/js.js
@@ -0,0 +1,28 @@
+(function wireNextGuideButton(step) {
+    step && step.attachEvent(step.guideElement[0], 'click', function(e) {
+        var advanceButton = pendo.dom(e.target || e.srcElement).closest('.opt1');
+        if (advanceButton.length) {
+
+            // Use the below code to trigger another guide on the same page where this guide is targeted.
+            // This command dismisses the current active guide
+            pendo.onGuideDismissed();
+
+            // This command will show the guide of the ID provided
+            pendo.showGuideById('GUIDE-ID-HERE');
+            
+            /* ----------------------------------------------------- */
+
+            // Uncomment below lines to build your own dynamic permalink URL by using the metadata variable.
+            // get the text value of the fetched metadata to be used ahead.
+            
+            // var account_name = document.getElementsByClassName("_pendo-hidden-account_")[0].innerHTML
+            // var newurl = "https://" + account_name + ".app.com/?pendo=your_pendo_guide_id"
+            
+            // //Finally dismissing the current guide to trigger the permalink.
+            // pendo.onGuideDismissed();
+            
+            // // Opening the dynamic permalink url in the same browser tab.
+            // window.open(newurl, "_self");
+        }
+    });
+})(step, guide);

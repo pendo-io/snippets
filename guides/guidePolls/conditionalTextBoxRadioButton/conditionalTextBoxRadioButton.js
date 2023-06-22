f9ec23439decb3addae60999e188cfdc345bc18c
Michael Fotinatos <mikefo@pendo.io>
2023-06-22 18:37:16 -0400

    Update conditionalTextBoxRadioButton.js
    Using improved pseudoselectors instead of nested parentElement references

--- guides/guidePolls/conditionalTextBoxRadioButton/conditionalTextBoxRadioButton.js
+++ guides/guidePolls/conditionalTextBoxRadioButton/conditionalTextBoxRadioButton.js
@@ -6,18 +6,18 @@ var textBoxQuestionText = "Tell us more about your choice";
 var isTextBoxVisible = true;
 
 if(!pendo.designerEnabled) {
-    pendo.dom('._pendo-open-text-poll-wrapper')[0].parentElement.parentElement.parentElement.style.display = "none";
+    pendo.dom('._pendo-row:has(._pendo-open-text-poll-wrapper)')[0].style.display = "none";
     pendo.dom('.bb-text:contains('+ textBoxQuestionText +')')[0].style.display = "none";
     isTextBoxVisible = false;
 }
 
 pendo.dom('.pendo-radio').on('click', '.pendo-radio', function(e) {
     if (e.target.parentElement.innerText=="Other" && !isTextBoxVisible) {
-    	pendo.dom('._pendo-open-text-poll-wrapper')[0].parentElement.parentElement.parentElement.style.display = "block";
+    	pendo.dom('._pendo-row:has(._pendo-open-text-poll-wrapper')[0].style.display = "block";
     	pendo.dom('.bb-text:contains('+ textBoxQuestionText +')')[0].style.display = "block"; 
         isTextBoxVisible = true;
     } else if (e.target.parentElement.innerText!="Other" && isTextBoxVisible) {
-        pendo.dom('._pendo-open-text-poll-wrapper')[0].parentElement.parentElement.parentElement.style.display = "none";
+        pendo.dom('._pendo-row:has(._pendo-open-text-poll-wrapper')[0].style.display = "none";
         pendo.dom('.bb-text:contains('+ textBoxQuestionText +')')[0].style.display = "none";
         pendo.dom('._pendo-open-text-poll-input')[0].value="";
         isTextBoxVisible = false;

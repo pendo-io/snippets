c5374bed4a3c261d257c6271ea9f4287b1b43e54
krtb <kurtbauer55@protonmail.com>
2023-12-22 13:51:10 -0500

    Add closing breacket and closing parenthesis

--- guides/goToGuideStep/skipStepNoElement/skipStepsNoElement.js
+++ guides/goToGuideStep/skipStepNoElement/skipStepsNoElement.js
@@ -1,17 +1,17 @@
-//(function pendoCustomMain(){
+(function pendoCustomMain() {
     var logicCheckTimeoutAmmountInMilliseconds = 1500; //set as 0 if no delay is required
     var skippedNumberOfSteps = 1; //Number needs to be 1 or greater.
 
-    setTimeout(skipStepsNoElementLogic,Number(logicCheckTimeoutAmmountInMilliseconds));
+    setTimeout(skipStepsNoElementLogic, Number(logicCheckTimeoutAmmountInMilliseconds));
     function skipStepsNoElementLogic() {
-        if(!pendo.designerEnabled){
+        if (!pendo.designerEnabled) {
             pendo.log(`skipStepsNoElement function start main logic`);
             var guidePayload = pendo.getActiveGuide();
             var currentStepIndex = Number(guidePayload.stepIndex);
             var guideSteps = guidePayload.guide.steps;
             var nextStepPayload = guideSteps[currentStepIndex + 1];
             var nextStepId = guideSteps[currentStepIndex + 1].id;
-            if(guideSteps[currentStepIndex + 1 + skippedNumberOfSteps]){
+            if (guideSteps[currentStepIndex + 1 + skippedNumberOfSteps]) {
                 var skipToStepId = guideSteps[currentStepIndex + 1 + skippedNumberOfSteps].id;
             }
 
@@ -19,36 +19,37 @@
                 pendo.log(`skipStepsNoElement starting checkElementAndSkip function`);
                 var results = pendo.Sizzle(nextStepPayload.elementPathRule);
                 if (results.length === 0 || !pendo._.some(results, pendo.isElementVisible)) {
-                    if(skipToStepId){
+                    if (skipToStepId) {
                         pendo.log(guide.id + `:advance ${1 + skippedNumberOfSteps} steps`);
-                        pendo.goToStep({destinationStepId: skipToStepId});
-                    } else{
+                        pendo.goToStep({ destinationStepId: skipToStepId });
+                    } else {
                         pendo.log(`skipStepNoElement did not find any remaining guide steps. dismissing guide.`);
                         pendo.onGuideDismissed();
                     }
-                    
+
                 } else {
                     pendo.log(guide.id + ':advance 1 step');
-                    pendo.goToStep({destinationStepId: nextStepId});
+                    pendo.goToStep({ destinationStepId: nextStepId });
                 }
             })();
         }
     };
 
     var invisibleStepStylingFixTimeoutAmmountInMilliseconds = 1;
-    setTimeout(invisibleStepStylingFix,Number(invisibleStepStylingFixTimeoutAmmountInMilliseconds));
-    function invisibleStepStylingFix(){
-        if(!pendo.designerEnabled){
-            if(pendo.dom(`#pendo-guide-container`)[0]){
-                if(pendo._.has(pendo.dom(`#pendo-guide-container`)[0].style, "display")){
+    setTimeout(invisibleStepStylingFix, Number(invisibleStepStylingFixTimeoutAmmountInMilliseconds));
+    function invisibleStepStylingFix() {
+        if (!pendo.designerEnabled) {
+            if (pendo.dom(`#pendo-guide-container`)[0]) {
+                if (pendo._.has(pendo.dom(`#pendo-guide-container`)[0].style, "display")) {
                     pendo.dom(`#pendo-guide-container`)[0].style.display = "none";
                 }
             }
-            if(pendo.dom(`#pendo-backdrop`)[0]){
-                if(pendo._.has(pendo.dom(`#pendo-backdrop`)[0].style, "display")){
+            if (pendo.dom(`#pendo-backdrop`)[0]) {
+                if (pendo._.has(pendo.dom(`#pendo-backdrop`)[0].style, "display")) {
                     pendo.dom(`#pendo-backdrop`)[0].style.display = "none";
                 }
             }
             pendo.log(`invisible step styling fixed (guide container display removed & backdrop display removed for invisible step)`)
         }
     }
+})

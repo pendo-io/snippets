c8ee37850fb47ecca0a876ef528fb0aad24de36b
Mike Fotinatos <mikefo@pendo.io>
2024-02-18 20:14:40 -0500

    adding simple carousel in addition to robust one -- can't actually get robust one to work :/

---
+++ guides/customGuideAssets/carousel/carousel_simple/carousel.js
@@ -0,0 +1,14 @@
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});

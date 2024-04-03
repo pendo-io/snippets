11c111e6756e1ce971b150ee12c336ac07399b58
Kurt Bauer <kurt.bauer@pendo.io>
2024-04-03 15:08:01 -0400

    Update updateMetadataDropdown.js

--- guides/guidePolls/submitPollResponseAsMetadata/updateMetadataDropdown.js
+++ guides/guidePolls/submitPollResponseAsMetadata/updateMetadataDropdown.js
@@ -7,7 +7,8 @@ var personaSelect = document.querySelector('._pendo-multi-choice-poll-select');
 document.querySelector('[id^="pendo-button"]').addEventListener('click', updatePersona);
 
 function updatePersona(){    
-    // create visitor agent metadata field called 'persona'
+    // create visitor agent metadata field called 'persona', this property can be updated to satifsfy different naming requirements
+    // 'userRole' is the value pulled from available options in the multi-select poll. Update the poll to provide different values.
     let userRole = personaSelect.options[personaSelect.selectedIndex].text;
     const account = window.pendo.getSerializedMetadata().account;
     const visitor = window.pendo.getSerializedMetadata().visitor;

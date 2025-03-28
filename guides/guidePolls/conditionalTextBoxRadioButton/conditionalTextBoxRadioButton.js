45c1906fc4a99a307c7efb681feff6f208f28da3
Mike Fotinatos <mikefo@pendo.io>
2025-03-27 20:36:05 -0400

    adding new snippets and documentation for pro

--- guides/guidePolls/conditionalTextBoxRadioButton/conditionalTextBoxRadioButton.js
+++ guides/guidePolls/conditionalTextBoxRadioButton/conditionalTextBoxRadioButton.js
@@ -1,25 +1,36 @@
-// INSTRUCTIONS: Be sure to change the value for "textBoxQuestionText" below to the text directly above your
-// open text poll.  All other functionality should operate normally.  Note that this functionality should
-// always be contained within its own guide step, or it may affect other polls in the guide.
-
-var textBoxQuestionText = "Tell us more about your choice";
+var textBoxQuestionText = "[hidden]";
 var isTextBoxVisible = true;
 
 if(!pendo.designerEnabled) {
     pendo.dom('._pendo-row:has(._pendo-open-text-poll-wrapper)')[0].style.display = "none";
-    pendo.dom('.bb-text:contains('+ textBoxQuestionText +')')[0].style.display = "none";
-    isTextBoxVisible = false;
+    var hiddenTextArea = pendo.dom('.bb-text:contains('+ textBoxQuestionText +')')[0];
+    hiddenTextArea.style.display = "none";
+    hiddenTextArea.setAttribute("hiddenTextArea", "true");
+    hiddenTextArea.textContent = hiddenTextArea.textContent.replace("[hidden]", "").trim(); 
+    isTextBoxVisible = false; 
+
+    for(var i=0; i < pendo.dom(".pendo-radio:contains('[showHidden]')").length; i++) {
+        var elem = pendo.dom(".pendo-radio:contains('[showHidden]')")[i];
+        elem.parentElement.setAttribute("showHidden", "true");
+        elem.textContent=elem.textContent.replace("[showHidden]", "").trim(); 
+    }
 }
 
-pendo.dom('.pendo-radio').on('click', '.pendo-radio', function(e) {
-    if (e.target.parentElement.innerText=="Other" && !isTextBoxVisible) {
-    	pendo.dom('._pendo-row:has(._pendo-open-text-poll-wrapper')[0].style.display = "block";
-    	pendo.dom('.bb-text:contains('+ textBoxQuestionText +')')[0].style.display = "block"; 
+
+for(var i=0; i < pendo.dom(".pendo-radio").length; i++) {
+  // Loop through each element and attach a click event listener
+  pendo.dom(".pendo-radio")[i].addEventListener('click', (e) => {
+    debugger;
+    if ((e.target.parentElement.getAttribute("showhidden") || e.target.getAttribute("showhidden")) && !isTextBoxVisible) {
+        pendo.dom('._pendo-row:has(._pendo-open-text-poll-wrapper)')[0].style.display = "block";
+        pendo.dom('[hiddenTextArea')[0].style.display = "block"; 
         isTextBoxVisible = true;
-    } else if (e.target.parentElement.innerText!="Other" && isTextBoxVisible) {
-        pendo.dom('._pendo-row:has(._pendo-open-text-poll-wrapper')[0].style.display = "none";
-        pendo.dom('.bb-text:contains('+ textBoxQuestionText +')')[0].style.display = "none";
+    } else if ((!e.target.parentElement.getAttribute("showhidden") && !e.target.getAttribute("showhidden")) && isTextBoxVisible) {
+        pendo.dom('._pendo-row:has(._pendo-open-text-poll-wrapper)')[0].style.display = "none";
+        pendo.dom('[hiddenTextArea')[0].style.display = "none";
         pendo.dom('._pendo-open-text-poll-input')[0].value="";
-        isTextBoxVisible = false;
+        isTextBoxVisible = false; 
     }
-});
+  });
+};
+

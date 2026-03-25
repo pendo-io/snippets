1fb336c621705fcc2110b7a4532e3ce7e991fd15
nikhilsathyanarayana-lab <nikhil.sathyanarayana@pendo.io>
2026-03-25 14:34:53 +0000

    Add clickAnywhere functionality to handle clicks

---
+++ guides/advancingGuides/clickAnywhere/js.js
@@ -0,0 +1,18 @@
+function handleClick(event) {
+  // Ignore clicks inside any Pendo guide
+  if (event.target.closest('[id^="pendo-"], [class*="_pendo-"], .pendo-guide')) {
+    return;
+  }
+
+  if (hasAdvanced) return;
+
+  hasAdvanced = true;
+
+  try {
+    pendo.onGuideAdvanced();
+  } catch (e) {}
+
+  parent.document.removeEventListener('click', handleClick, true);
+}
+
+parent.document.addEventListener('click', handleClick, true);

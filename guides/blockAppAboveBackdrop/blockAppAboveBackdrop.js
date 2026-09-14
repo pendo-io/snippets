6a37ca60f4b7373c11cb12e1141cfacc2a0bb1b0
christophschumacher-spec <christoph.schumacher@pendo.io>
2026-09-14 16:20:15 -0400

    Create blockAppAboveBackdrop.js
    JS snippet to add a div element that covers the part of the screen shown above the backdrop. This will block users from interacting with the elements that are visible above the backdrop. Prevents users from performing navigation that would disrupt the flow of a multi-step guide.

---
+++ guides/blockAppAboveBackdrop/blockAppAboveBackdrop.js
@@ -0,0 +1,36 @@
+(function (step) {
+    if (!pendo.designerEnabled) {
+        const p_backdrop = document.querySelector('#pendo-backdrop');
+        if (p_backdrop) {
+            // console.log("Backdrop found.");
+            const p_open = pendo.dom(step.elementPathRule)[0]; // section shown above backdrop
+            const p_top_backdrop = document.querySelector('.pendo-backdrop-region-top'); // top secton of backdrop
+
+            const top_height = p_top_backdrop.getBoundingClientRect().height + "px"; // get height of top section
+            const top_pos_left = p_top_backdrop.getBoundingClientRect().left + "px"; // get left position offset of top section
+            
+            const open_section_height = p_open.getBoundingClientRect().height +"px";// get height of section shown above backdrop
+            const open_section_width = p_open.getBoundingClientRect().width + "px";// get width of section shown above backdrop
+            
+            // construct additional backdrop div to cover open section
+            // console.log(top_height, top_pos_left, open_section_height, open_section_width);
+            
+            const open_section = document.createElement('div');
+            open_section.className = "_pendo-backdrop";
+            Object.assign(open_section.style, {
+                'top': top_height, // use height of existing top backdrop as offset
+                'height': open_section_height, // cover whole height of open section
+                'left': top_pos_left, // left offset equal to top section of backdrop
+                'width': open_section_width, // cover whole width of open section
+                'z-index': '200000', // adjust if needed
+                'position': 'fixed',
+                'opacity': '0', // sets opacity to 0 so elements behind it are visible
+                'pointer-events': 'all',
+                'float': 'none',
+                'vertical-align': 'baseline',
+                'display': 'block'
+            });
+            p_backdrop.appendChild(open_section); // add new div to backdrop section of the guide DOM
+      }
+  }
+})(step, guide);
